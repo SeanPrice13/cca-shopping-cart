e9c926287dac6a9ce8da080ba90fc7174f1d8664
SeanPrice13 <bentleyprice@live.com>
2022-03-18 13:14:32 -0400

    Updated App.js to include React.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Basket from './components/Basket';
 import Header from './components/Header';
