4e4bf3e11c9ba58e38152699c85fcfd2dea1fc28
SeanPrice13 <bentleyprice@live.com>
2022-03-18 07:57:36 -0400

    Initial Commit

---
+++ src/services/productsService.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            id: 1,
+            name: '6" Beef Sub',
+            price: 11.25,
+            image: require('../assets/products/sub-115.jpg'),
+        },
+        {
+            id: 2,
+            name: '6" Meatball Sub',
+            price: 9.50,
+            image: require('../assets/products/sub-116.jpg'),
+        },
+        {
+            id: 3,
+            name: '6" Tuna Sub',
+            price: 12.75,
+            image: require('../assets/products/sub-117.jpg'),
+        },
+    ],
+};
+
+export default data;
